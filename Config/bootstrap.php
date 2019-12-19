a0c082447c808cfb4914592a2766bdb7e649f099
Gregor <gregor-j@users.noreply.github.com>
2019-12-19 16:30:36 +0100

    initial commit

---
+++ Config/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Get the current application version either from the given file, a default file
+ * location or from the current GIT repository.
+ * @param string|null $file Manually define the commit.json file. Default: null
+ * @return string
+ */
+function appVersion($file = null)
+{
+    if ($file === null) {
+        $file = APP.DS.'webroot'.DS.'commit.json';
+    }
+    static $version;
+    static $versionFile;
+    if ($version === null || $file !== $versionFile) {
+        $version = new \kbATeam\Version\FileVersion($file);
+        if ($version->exists()) {
+            $versionFile = $file;
+        } else {
+            $version = new \kbATeam\Version\GitVersion(APP);
+        }
+    }
+    if ($version->exists()) {
+        return sprintf(
+            '%s (rev. %s)',
+            $version->getBranch(),
+            $version->getCommit()
+        );
+    }
+    return '~ ? ~';
+}
+define('APP_VERSION', appVersion());
